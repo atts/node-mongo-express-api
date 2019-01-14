a08ad14e225962e82b0d3717d1f1f9bc74eaac25
Atul Mishra <atul.mishra0590@gmail.com>
2019-01-14 23:31:13 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-mongo-express-api
-the following code facilitates the CRUD operations in a MongoDB database using Express Api
+## the following code facilitates the CRUD operations in a MongoDB database using Express Api
+
+ ### Run below commands in same order.
+
+ 1. npm install
+ 2. npm start
+
+ ## open browser and go to URL http://locahost:3000/ to access your api. You can access to specific routes as defined in code by adding the route after the this URL
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('mongojs');
+const db = mongo('<name of database>', ['<name of collection>']);
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log("listening to port : " + port);
+});
+
+// default route
+app.get('/', (req, res, next) => {
+    res.send("api running...");
+});
+
+// route to read the document data from collection
+app.get('/api/products', (req, res, next) => {
+    db.products.find((err, docs) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(docs);
+    });
+})
+
+// route to read particular document from collection using ID
+app.get('/api/products/:id', (req, res, next) => {
+    db.products.findOne({ _id: mongo.ObjectId(req.params.id) }, (err, doc) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(doc);
+    });
+})
+
+// route to insert the document data into collection
+app.post('/api/products', (req, res, next) => {
+    db.products.insert(req.body, (err, doc) => {
+        if (err) {
+            res.send(err)
+        };
+        res.json(doc);
+    });
+})
+
+// route to update the document data using ID
+app.put('/api/products/:id', (req, res, next) => {
+    db.products.findAndModify({
+        query: { _id: mongo.ObjectId(req.params.id) },
+        update: {
+            $set: {
+                name: req.body.name,
+                category: req.body.category
+            }
+        },
+        new: true
+    }, (err, doc) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(doc);
+    })
+})
